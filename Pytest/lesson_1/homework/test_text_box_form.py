f7e77191fb5e4c103b05ef068cf9716ac33b1fec
bugaev <bugaev1404g@mail.ru>
2026-06-27 16:12:49 +0300

    Добавил использование датаклассов в каждый тест и ассерт

--- Pytest/lesson_1/homework/test_text_box_form.py
+++ Pytest/lesson_1/homework/test_text_box_form.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 
-from text_box_form import TextBoxForm
+from text_box_form import TextBoxForm, UserData
 
 
 class TestTextBoxForm:
@@ -26,8 +26,14 @@ class TestTextBoxForm:
         ("Bugaev Dmitry", "BUGAEV@EXAMPLE.COM", "New-York, wall-street", "456 Oak St")
     ])
     def test_positive_data(self, text_box_form, full_name, email, current_address, permanent_address):
-        text_box_form.fill_full_form(full_name, email, current_address, permanent_address)
-        text_box_form.assert_valid_result(full_name, email, current_address, permanent_address)
+        user_data = UserData(
+            name=full_name,
+            email=email,
+            current_address=current_address,
+            permanent_address=permanent_address
+        )
+        text_box_form.fill_full_form(user_data)
+        text_box_form.assert_valid_result(user_data)
 
     @pytest.mark.parametrize("full_name, email, current_address, permanent_address", [
         ("Bugaev Dmitry", "", "", ""),
@@ -37,8 +43,14 @@ class TestTextBoxForm:
         ("Bugaev Dmitry", "bugaev@example.com", "", "")
     ])
     def test_partial_form_submission(self, text_box_form, full_name, email, current_address, permanent_address):
-        text_box_form.fill_full_form(full_name, email, current_address, permanent_address)
-        text_box_form.assert_valid_result(full_name, email, current_address, permanent_address)
+        user_data = UserData(
+            name=full_name,
+            email=email,
+            current_address=current_address,
+            permanent_address=permanent_address
+        )
+        text_box_form.fill_full_form(user_data)
+        text_box_form.assert_valid_result(user_data)
 
     # @pytest.mark.parametrize("email", [
     #     "bugaevexample.com",
@@ -54,9 +66,9 @@ class TestTextBoxForm:
     #     print(text_box_form.get_validation_message())
     #     text_box_form.assert_invalid_email(email=email)
 
-    @pytest.mark.parametrize("email, error_message", [
+    @pytest.mark.parametrize("email, expected_error_message", [
         ("bugaevexample.com",
-         '"Адрес электронной почты должен содержать символ "@". В адресе "bugaevexample.com" отсутствует символ "@".'),
+         'Адрес электронной почты должен содержать символ "@". В адресе "bugaevexample.com" отсутствует символ "@".'),
         # "bugaev@examplecom", Форма пропускает данный email
         ("@example.com", 'Введите часть адреса до символа "@". Адрес "@example.com" неполный.'),
         ("bugaev@@example.com", 'Часть адреса после символа "@" не должна содержать символ "@".'),
@@ -65,20 +77,28 @@ class TestTextBoxForm:
         ("bugaevexample",
          'Адрес электронной почты должен содержать символ "@". В адресе "bugaevexample" отсутствует символ "@".')
     ])
-    def test_invalid_email(self, text_box_form, email, error_message):
-        text_box_form.fill_full_form(email=email)
-        print(text_box_form.get_validation_message())
-        text_box_form.assert_invalid_email(email=email, error_message=error_message)
+    def test_invalid_email(self, text_box_form, email, expected_error_message):
+        user_data = UserData(
+            email=email,
+        )
+        text_box_form.fill_full_form(user_data)
+        text_box_form.assert_invalid_email(user_data, expected_error_message)
 
     @pytest.mark.parametrize("full_name, email, current_address, permanent_address", [
-        (f"{"Bugaev Dmitry" * 100}", "bugaev@example.com", "New-York, wall-street", "456 Oak St"),
-        ("Bugaev Dmitry", f"{"bugaev" * 11}@example.com", "New-York, wall-street", "456 Oak St"),
-        ("Bugaev Dmitry", "bugaev@example.com", f"{"New-York, wall-street" * 100}", "456 Oak St"),
-        ("Bugaev Dmitry", "bugaev@example.com", "New-York, wall-street", f"{"456 Oak St" * 100}")
+        ("Bugaev Dmitry" * 100, "bugaev@example.com", "New-York, wall-street", "456 Oak St"),
+        ("Bugaev Dmitry", "bugaev" * 11 + "@example.com", "New-York, wall-street", "456 Oak St"),
+        ("Bugaev Dmitry", "bugaev@example.com", "New-York, wall-street" * 100, "456 Oak St"),
+        ("Bugaev Dmitry", "bugaev@example.com", "New-York, wall-street", "456 Oak St" * 100)
     ])
     @pytest.mark.xfail
     def test_long_data(self, text_box_form, full_name, email, current_address, permanent_address):
-        text_box_form.fill_full_form(full_name, email, current_address, permanent_address)
+        user_data = UserData(
+            name=full_name,
+            email=email,
+            current_address=current_address,
+            permanent_address=permanent_address
+        )
+        text_box_form.fill_full_form(user_data)
         text_box_form.assert_long_data()
 
     @pytest.mark.parametrize("full_name, email, current_address, permanent_address", [
@@ -86,7 +106,13 @@ class TestTextBoxForm:
     ])
     @pytest.mark.xfail
     def test_empty_fields(self, text_box_form, full_name, email, current_address, permanent_address):
-        text_box_form.fill_full_form(full_name, email, current_address, permanent_address)
+        user_data = UserData(
+            name=full_name,
+            email=email,
+            current_address=current_address,
+            permanent_address=permanent_address
+        )
+        text_box_form.fill_full_form(user_data)
         text_box_form.assert_empty_fields()
 
     @pytest.mark.parametrize("security_payload", [
@@ -96,5 +122,8 @@ class TestTextBoxForm:
         "<div>HTML injection</div>"
     ])
     def test_security_payload(self, text_box_form, security_payload):
-        text_box_form.fill_full_form(full_name=security_payload)
-        text_box_form.assert_security_payload(security_payload)
+        user_data = UserData(
+            name=security_payload,
+        )
+        text_box_form.fill_full_form(user_data)
+        text_box_form.assert_security_payload(user_data)
