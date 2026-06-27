fe8e6b749dc2452dfc47a886d34e67d254e6cc95
bugaev <bugaev1404g@mail.ru>
2026-06-27 16:12:02 +0300

    text_box_form: Исправил логику отработки случаев с None, добавил использование датаклассов в каждый ассерт

--- Pytest/lesson_1/homework/text_box_form.py
+++ Pytest/lesson_1/homework/text_box_form.py
@@ -1,6 +1,24 @@
+from dataclasses import dataclass
+
 from selenium.webdriver.support.wait import WebDriverWait
 from seleniumpagefactory.Pagefactory import PageFactory
 from selenium.webdriver.support import expected_conditions as ec
+
+
+@dataclass
+class UserData:
+    name: str | None = None
+    email: str | None = None
+    current_address: str | None = None
+    permanent_address: str | None = None
+
+
+@dataclass
+class OutputData:
+    name: str | None = None
+    email: str | None = None
+    current_address: str | None = None
+    permanent_address: str | None = None
 
 
 class TextBoxForm(PageFactory):
@@ -34,44 +52,61 @@ class TextBoxForm(PageFactory):
         validation_message = self.email_input.get_attribute("validationMessage")
         return validation_message
 
-    def fill_full_form(self, full_name=None, email=None, current_address=None, permanent_address=None):
-        if full_name is not None:
+    def fill_full_name(self, full_name=None):
+        if full_name:
             self.full_name_input.send_keys(full_name)
-        if email is not None:
+
+    def fill_email(self, email=None):
+        if email:
             self.email_input.send_keys(email)
-        if current_address is not None:
+
+    def click_button(self):
+        self.submit_button.click()
+
+    def fill_current_address(self, current_address=None):
+        if current_address:
             self.current_address_input.send_keys(current_address)
-        if permanent_address is not None:
+
+    def fill_permanent_address(self, permanent_address=None):
+        if permanent_address:
             self.permanent_address_input.send_keys(permanent_address)
 
-        self.submit_button.click()
+    def fill_full_form(self, user_data: UserData):
+        self.fill_full_name(user_data.name)
+        self.fill_email(user_data.email)
+        self.fill_current_address(user_data.current_address)
+        self.fill_permanent_address(user_data.permanent_address)
+        self.click_button()
 
     def get_output_data(self):
         self.wait.until(ec.visibility_of(self.output_data))
-        full_name = self.output_name.text.replace("Name:", "").strip()
-        email = self.output_email.text.replace("Email:", "").strip()
-        current_address = self.output_cur_adr.text.replace("Current Address :", "").strip()
-        permanent_address = self.output_per_adr.text.replace("Permananet Address :", "").strip()
+        output_data = OutputData(
+            name=self.output_name.text.replace("Name:", "").strip(),
+            email=self.output_email.text.replace("Email:", "").strip(),
+            current_address=self.output_cur_adr.text.replace("Current Address :", "").strip(),
+            permanent_address=self.output_per_adr.text.replace("Permananet Address :", "").strip()
+        )
 
-        return {"full_name": full_name, "email": email, "current_address": current_address,
-                "permanent_address": permanent_address}
+        return output_data
 
-    def assert_valid_result(self, full_name=None, email=None, current_address=None, permanent_address=None):
+    def assert_valid_result(self, user_data: UserData):
         actual_data = self.get_output_data()
 
-        if full_name is not None:
-            assert actual_data["full_name"] == full_name.strip(), "Введенные данные не совпадают!"
-        if email is not None:
-            assert actual_data["email"] == email.strip(), "Введенные данные не совпадают!"
-        if current_address is not None:
-            assert actual_data["current_address"] == current_address.strip(), "Введенные данные не совпадают!"
-        if permanent_address is not None:
-            assert actual_data["permanent_address"] == permanent_address.strip(), "Введенные данные не совпадают!"
+        if user_data.name:
+            assert actual_data.name == user_data.name.strip()
+        if user_data.email:
+            assert actual_data.email == user_data.email.strip()
+        if user_data.current_address:
+            assert actual_data.current_address == user_data.current_address.strip()
+        if user_data.permanent_address:
+            assert actual_data.permanent_address == user_data.permanent_address.strip()
 
-    def assert_invalid_email(self, email=None, error_message=None):
+    def assert_invalid_email(self, user_data: UserData, expected_error_message=None):
         email_error = self.get_validation_message()
-        if email is not None:
+        if user_data.email:
             assert len(email_error.strip()) > 0, "Форма пропустила невалидный email!"
+        if expected_error_message:
+            assert email_error == expected_error_message, f"Ожидаемое сообщение об ошибке: {expected_error_message}, не совпадает с полученным: {email_error}"
 
     def assert_long_data(self):
         assert not self.output_data.is_displayed(), "Форма пропустила длинное поле!"
@@ -79,7 +114,8 @@ class TextBoxForm(PageFactory):
     def assert_empty_fields(self):
         assert not self.output_data.is_displayed(), "Форма отправилась с пустыми полями!"
 
-    def assert_security_payload(self, security_payload):
+    def assert_security_payload(self, user_data: UserData):
         actual_data = self.get_output_data()
         assert actual_data is not None, "Форма упала при вводе небезопасных значений!"
-        assert actual_data["full_name"] == security_payload.strip(), "Введенные данные не совпадают!"
+        if user_data.name:
+            assert actual_data.name == user_data.name.strip(), "Введенные данные не совпадают!"
